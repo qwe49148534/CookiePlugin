99a5116ac4920c6f90e3d859863bdf1c38b75455
YouKlike <w93816@gmail.com>
2021-05-25 21:13:21 +0800

    專案初始化提交

---
+++ src/main/java/cookie/main/cookieplugin/CookiePlugin.java
@@ -0,0 +1,40 @@
+/*-
+ * LICENSE
+ * CooKiePlugin
+ * -------------
+ * Copyright (C) 2021 YouKlike
+ * -------------
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ */
+
+package cookie.main.cookieplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CookiePlugin
+        extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
